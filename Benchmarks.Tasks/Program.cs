1b389d9e493f85e7e98a5ece9c6620e7c132ce3c
Савелий Вотинцев <votintcev1@mail.ru>
2025-01-11 15:26:17 +0300

    Новые сценарии
    - Добавлены методы без await

--- Benchmarks.Tasks/Program.cs
+++ Benchmarks.Tasks/Program.cs
@@ -42,6 +42,32 @@ public class TaskVsValueTaskBenchmark
         await Task.WhenAll(tasks);
     }
 
+    [Benchmark]
+    public Task MeasureTaskPerformanceWithoutAwait()
+    {
+        List<Task> tasks = new(Iterations);
+
+        for (int i = 0; i < Iterations; i++)
+        {
+            tasks.Add(Task.Run(TaskMethodWithoutAwait));
+        }
+
+        return Task.WhenAll(tasks);
+    }
+
+    [Benchmark]
+    public Task MeasureValueTaskPerformanceWithoutAwait()
+    {
+        List<Task> tasks = new(Iterations);
+
+        for (int i = 0; i < Iterations; i++)
+        {
+            tasks.Add(Task.Run(ValueTaskMethodWithoutAwait));
+        }
+
+        return Task.WhenAll(tasks);
+    }
+
     private async Task TaskMethod()
     {
         int result = Math.Abs(Random.Shared.Next(0, 100000));
@@ -52,5 +78,17 @@ public class TaskVsValueTaskBenchmark
     {
         int result = Math.Abs(Random.Shared.Next(0, 100000));
         await ValueTask.CompletedTask;
+    }
+
+    private Task TaskMethodWithoutAwait()
+    {
+        int result = Math.Abs(Random.Shared.Next(0, 100000));
+        return Task.CompletedTask;
+    }
+
+    private ValueTask ValueTaskMethodWithoutAwait()
+    {
+        int result = Math.Abs(Random.Shared.Next(0, 100000));
+        return ValueTask.CompletedTask;
     }
 }
